af191586baf9bc90b6b1f880c9e79c6ae2112d38
Hiromitsu Oshiba <h.oshiba.0117@gmail.com>
2017-09-02 20:28:30 +0900

    ADD: period limitter

--- lmttr.go
+++ lmttr.go
@@ -2,6 +2,7 @@ package lmttr
 
 import (
 	"sync"
+	"time"
 )
 
 // Limitter object
@@ -33,4 +34,59 @@ func (l *Limitter) End() {
 // Wait all processes
 func (l *Limitter) Wait() {
 	l.wait.Wait()
+}
+
+// PeriodLimitter provides limit in period
+type PeriodLimitter struct {
+	semaphore chan bool
+	wait      sync.WaitGroup
+	period    *time.Ticker
+}
+
+// NewPeriodLimitter from concurrency and span
+func NewPeriodLimitter(concurrency uint, span time.Duration) (p PeriodLimitter, err error) {
+
+	s := make(chan bool, concurrency)
+	ticker := time.NewTicker(span)
+
+	go func() {
+
+		for range ticker.C {
+
+			for {
+
+				select {
+				case <-s:
+					// take value while it possible
+				default:
+					break
+				}
+
+			}
+
+		}
+
+	}()
+
+	return PeriodLimitter{
+		semaphore: s,
+		wait:      sync.WaitGroup{},
+		period:    ticker,
+	}, nil
+}
+
+// Start of a process
+func (pl *PeriodLimitter) Start() {
+	pl.semaphore <- true
+	pl.wait.Add(1)
+}
+
+// End of a process
+func (pl *PeriodLimitter) End() {
+	pl.wait.Done()
+}
+
+// Wait to finish all of processes started
+func (pl *PeriodLimitter) Wait() {
+	pl.wait.Wait()
 }
